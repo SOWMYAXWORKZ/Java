daa7f49a98e0159299bcccc2c2ac7a29e7146304
SOWMYAXWORKZ <sowmyamvs.xworkz@gmail.com>
2024-06-20 14:23:27 +0530

    hero class

--- Hero.java
+++ Hero.java
@@ -3,7 +3,14 @@ class Hero{
 	
 	
 	System.out.println("HERO: Prabhas");
-	
+	// git status
+	// git add ./
+	// git status
+	// git commit -m "20-06-2024" if error came use the below cmd
+	// git config --global user.email "sowmyamvs.xworkz@gmail.com"
+	// git config -l
+	// git config --global user.name "SOWMYAXWORKZ"
+	// to conform tht uese
 	
 	
 	
